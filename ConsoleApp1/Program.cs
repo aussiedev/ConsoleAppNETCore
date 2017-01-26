5a01cebf15615c4bf5ff75b3682c02fe0f749845
Kans Parmar <parmarkanaiya@gmail.com>
2017-01-26 17:43:24 +1100

    First checkin

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is hello from .NET Core");
+            Console.ReadLine();
+
+        }
+    }
+}
